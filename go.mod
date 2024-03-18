6ef25e4e1a4a98b13704e36dcdfa5ea816d655a0
Alain <yinxulai@hotmail.com>
2024-03-18 22:12:57 +0800

    init

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/yinxulai/donzo
+
+go 1.22.1
---
+++ internal/proxy/proxy.go
@@ -0,0 +1,10 @@
+package proxy
+
+import (
+	"log"
+)
+
+func StartHttpProxyServer() {
+	// 启动代理服务器
+	log.Println("代理服务器正在运行，监听端口8080...")
+}
